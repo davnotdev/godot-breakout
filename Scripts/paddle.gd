9e2cc0ef58007fdd9f33d6e9b37cf9e82f61d519
dav <usr21p1@gmail.com>
2023-10-23 13:27:16 -0700

    Initial Commit

---
+++ Scripts/paddle.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+class_name Paddle
+
+var direction := Vector2.ZERO
+var camera_rect: Rect2
+var half_paddle_width: float
+var is_ball_started = false
+
+@export var speed = 300
+@export var camera: Camera2D
+
+@onready var ball: Ball = $"../Ball"
+@onready var collision_shape = $CollisionShape2D
+
+func _ready():
+	camera_rect = camera.get_viewport_rect()
+	half_paddle_width = collision_shape.shape.get_rect().size.x / 2 * scale.x
+
+func _process(_delta):
+	var camera_start_x = camera.position.x - camera_rect.size.x / 2
+	var camera_end_x = camera.position.x + camera_rect.size.x / 2
+
+	if global_position.x - half_paddle_width < camera_start_x:
+		global_position.x = camera_start_x + half_paddle_width
+	elif global_position.x + half_paddle_width > camera_end_x:
+		global_position.x = camera_end_x - half_paddle_width
+
+func _physics_process(_delta):
+	linear_velocity = speed * direction
+
+func _input(_ev):
+	if Input.is_action_pressed("left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("right"):
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
+
+	if direction != Vector2.ZERO and !is_ball_started:
+		ball.start_ball()
+		is_ball_started = true
+		pass
+
