42e3f318c9d5d11d325149eae0894f1d815f5d95
dav <usr21p1@gmail.com>
2023-11-03 20:40:11 -0700

    Finally Fix Brick Spacing

--- Scripts/brick_spawner.gd
+++ Scripts/brick_spawner.gd
@@ -22,11 +22,14 @@ func _ready():
 func spawn():
 	var test_brick = brick_scene.instantiate() as Brick
 	add_child(test_brick)
-	var brick_size = test_brick.get_size()
+	var brick_size = test_brick.get_size() * 4
 	test_brick.queue_free()
 
 	var start_x = (
-		spawn_start.position.x - brick_size.x * (ROWS / 2.0) - floorf(ROWS / 2.0) * margin.x
+		spawn_start.position.x
+		- brick_size.x * (ROWS / 2.0)
+		- margin.x * (ROWS / 2.0)
+		+ brick_size.x / 2.0
 	)
 
 	for i in COLUMNS:
@@ -35,23 +38,12 @@ func spawn():
 			add_child(brick)
 			brick.set_level(COLUMNS - i)
 			brick.set_position(
-				Vector2(start_x + margin.x * j, spawn_start.position.y + margin.y * i)
+				Vector2(
+					start_x + (margin.x + brick_size.x) * j,
+					spawn_start.position.y + (margin.y + brick_size.y) * i
+				)
 			)
 			brick.brick_destroyed.connect(on_brick_destroyed)
-
-	# var row_width = brick_size.x * COLUMNS + margin.x * (COLUMNS - 1)
-	# var spawn_position_x = (-row_width + brick_size.x + margin.x) / 2
-	# var spawn_position_y = spawn_start.position.y
-
-	# for j in ROWS:
-	# for i in COLUMNS:
-	# var brick = brick_scene.instantiate() as Brick
-	# add_child(brick)
-	# brick.set_level(ROWS - j)
-	# var x = spawn_position_x + i * (margin.x + brick.get_size().x)
-	# var y = spawn_position_y + j * (margin.y + brick.get_size().y)
-	# brick.set_position(Vector2(x, y))
-	# brick.brick_destroyed.connect(on_brick_destroyed)
 
 
 func on_brick_destroyed():
