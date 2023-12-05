99dcf5dbf575fb96a271cec28a2a0188eac87ff2
dav <usr21p1@gmail.com>
2023-12-05 12:14:56 -0800

    Fix camera shake

--- Scripts/screen_shaker.gd
+++ Scripts/screen_shaker.gd
@@ -3,15 +3,19 @@ extends Camera2D
 signal wall_hit
 
 var screen_shake = false
-
+var original_position: Vector2
 
 func _process(delta):
 	if screen_shake:
 		position.x = sin(Time.get_ticks_msec()) * 3.0
 		position.y = cos(Time.get_ticks_msec()) * 2.0
+	else:
+		position = original_position
+
 
 
 func _on_ball_wall_hit():
 	screen_shake = true
+	original_position = position
 	await get_tree().create_timer(0.05).timeout
 	screen_shake = false
